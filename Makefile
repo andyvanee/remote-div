3853fbc0b6852e4778cad1136ab9a454e020e982
Andy VanEe <1andyvanee@gmail.com>
2017-05-18 23:19:23 -0600

    Add polyfill and v1 syntax

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
+SHELL:=/bin/bash
+
+RD=$(shell babel src/remote-div.js --presets es2016)
+CE=$(shell cat src/custom-elements.min.js)
 default: docs/remote-div.min.js docs/api/about.html
 
-docs/remote-div.min.js: src/remote-div.js
-	@uglifyjs $? > $@
+docs/remote-div.min.js: src/custom-elements.min.js src/remote-div.js
+	babel src/custom-elements.min.js src/remote-div.js --presets es2015 | uglifyjs > $@
 
 docs/api/about.html: README.md
 	@marked $? > $@
--- src/remote-div.js
+++ src/remote-div.js
@@ -1,27 +1,29 @@
 (function(){
-    var remote_div_proto = Object.create(HTMLDivElement.prototype);
+    class RemoteDiv extends HTMLElement {
+        constructor() {
+            super();
+        }
+        connectedCallback() {
+            var req = new XMLHttpRequest(),
+                remote_div = this,
+                src = this.getAttribute('data-src');
 
-    remote_div_proto.attachedCallback = function() {
-        var req = new XMLHttpRequest(),
-            remote_div = this,
-            src = this.getAttribute('data-src');
+            remote_div.setAttribute('data-stage', 'loading');
 
-        remote_div.setAttribute('data-stage', 'loading');
+            if (!src) return;
 
-        if (!src) return;
+            req.addEventListener('load', function() {
+                if (req.status == 200) {
+                    remote_div.innerHTML = req.responseText;
+                    remote_div.setAttribute('data-stage', 'complete');
+                } else {
+                    remote_div.setAttribute('data-stage', 'error');
+                }
+            });
 
-        req.addEventListener('load', function() {
-            if (req.status == 200) {
-                remote_div.innerHTML = req.responseText;
-                remote_div.setAttribute('data-stage', 'complete');
-            } else {
-                remote_div.setAttribute('data-stage', 'error');
-            }
-        });
-
-        req.open('GET', src, true);
-        req.send(null);
-    };
-
-    document.registerElement('remote-div', {prototype: remote_div_proto});
+            req.open('GET', src, true);
+            req.send(null);
+        }
+    }
+    customElements.define('remote-div', RemoteDiv);
 }());
